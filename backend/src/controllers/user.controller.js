fc531d33d6db486c0f791d771c00e97c56b2e187
aklesh-dev <arzunayavadu998@gmail.com>
2025-05-02 10:45:01 +0545

    Add Get Recommended Users and Friends Endpoints.

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/User.model.js";
+
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
+
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } },  // exclude current user
+        { $id: { $nin: currentUser.friends } },  // exclude current user's friend
+        { isOnBoarded: true },  // only include onboarded users
+      ]
+    });
+
+    res.status(200).json(recommendedUsers);
+
+  } catch (error) {
+    console.error("Error in getRecommendedUsers controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export async function getMyFriends(req, res) {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends")  // select only friends field
+      .populate("friends", "fullName, profilePic, nativeLanguage, learningLanguage"); // populate with user details
+
+    res.status(200).json(user.friends);
+
+  } catch (error) {
+    console.error("Error in getMyFriends controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
 import { connectDB } from './lib/db.config.js';
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 // Start server
 app.listen(PORT, () => {
