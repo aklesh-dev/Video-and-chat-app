ff9f7387c5871701d567187373ecf6c1d76e12e3
aklesh-dev <arzunayavadu998@gmail.com>
2025-05-02 11:12:06 +0545

    Send Friend Request Endpoint

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/FriendRequest.model.js";
 import User from "../models/User.model.js";
 
 export async function getRecommendedUsers(req, res) {
@@ -31,6 +32,54 @@ export async function getMyFriends(req, res) {
 
   } catch (error) {
     console.error("Error in getMyFriends controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export async function sendFriendRequest(req, res) {
+  try {
+    const myId = req.user.id;
+    const { id: recipientId } = req.params;
+
+    //? Prevent self-friendship
+    if (myId === recipientId) {
+      return res.status(400).json({ message: "You cannot send a friend request to yourself" });
+    }
+
+    //? Check if recipient exists
+    const recipient = await User.findById(recipientId);
+    if (!recipient) {
+      return res.status(400).json({ message: "Recipient not found" })
+    }
+
+    // ?Check if user is already friend
+    if (recipient.friends.includes(myId)) {
+      return res.status(400).json({ message: "You are already friends with this user" });
+    }
+
+    // ?Check if req already exists
+    const existingRequest = await FriendRequest.findOne({
+      $or: [
+        { sender: myId, recipient: recipientId },
+        { sender: recipientId, recipient: myId },
+      ],
+    });
+    if (existingRequest) {
+      return res.status(400).json({ message: "Friend request already sent" });
+    }
+
+    // ?Create new friend request
+    const friendRequest = new FriendRequest({
+      sender: myId,
+      recipient: recipientId,
+    });
+
+    await friendRequest.save();
+
+    res.status(201).json(friendRequest);
+
+  } catch (error) {
+    console.error("Error in sendFriendRequest controller: ", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
